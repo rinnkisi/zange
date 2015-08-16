770fa10d539e9b677f66d0755dbc920a8f8227af
user <user@user-no-MacBook-Air-4.local>
2015-08-16 21:54:42 +0900

    データベース取得処理を追加

--- Zange/SwiftDataSample.swift
+++ Zange/SwiftDataSample.swift
@@ -5,6 +5,7 @@
 //  Created by user on 2015/08/16.
 //  Copyright (c) 2015年 teamA. All rights reserved.
 //
+import UIKit
 
 class SwiftDataSample {
   
@@ -24,6 +25,23 @@ class SwiftDataSample {
         //there was an error during this function, handle it here
       } else {
         //no error, the table was created successfully
+        // データを追加
+        let Men = 1
+        let Women = 0
+        var gazou_id = 1;
+        
+        // 男性の画像追加
+        let data_men = NSString(contentsOfFile: NSBundle.mainBundle().pathForResource("men", ofType: "txt")!, encoding: NSUTF8StringEncoding, error: nil) as! String
+        data_men.enumerateLines { (line, stop) -> () in
+          Add_Gazou(gazou_id++, filename : line, sex: Men)
+        }
+        
+        // 女性の画像追加
+        let data_women = NSString(contentsOfFile: NSBundle.mainBundle().pathForResource("women", ofType: "txt")!, encoding: NSUTF8StringEncoding, error: nil) as! String
+        data_women.enumerateLines { (line, stop) -> () in
+          Add_Gazou(gazou_id++, filename : line, sex: Women)
+        }
+        
       }
     }
     // 格言テーブル
@@ -32,9 +50,95 @@ class SwiftDataSample {
         //there was an error during this function, handle it here
       } else {
         //no error, the table was created successfully
+        // データを追加
+        var kakugen_id = 1;
+        let data = NSString(contentsOfFile: NSBundle.mainBundle().pathForResource("proverbs", ofType: "txt")!, encoding: NSUTF8StringEncoding, error: nil) as! String
+        data.enumerateLines { (line, stop) -> () in
+          Add_Kakugen(kakugen_id++, kakugen: line)
+        }
       }
     }
     println(SD.databasePath())
+
   }
   
+  func Add_Kakugen(kakugen_id : Int, kakugen :String) ->Bool{
+    // 格言を格納する。
+    // sqlを準備
+    let sql = "INSERT INTO kakugen_mst (kakugen_id, word) VALUES (?, ?)";
+    
+    // ?に入る変数をバインドして実行
+    if let err = SwiftData.executeChange( sql, withArgs: [kakugen_id, kakugen]) {
+      //エラーが発生した時の処理
+      let msg = SwiftData.errorMessageForCode(err);
+      print(msg)
+      return false;
+    } else {
+      return true;
+    }
+  }
+  
+  func Add_Gazou(gazou_id : Int, filename : String, sex : Int) ->Bool{
+    // 画像のパスを格納する
+    // sqlを準備
+    let sql = "INSERT INTO gazou_mst (gazou_id, url, collect, sex) VALUES (?, ?, 0, ?)";
+    
+    // ?に入る変数をバインドして実行
+    if let err = SwiftData.executeChange( sql, withArgs: [gazou_id, filename, sex]) {
+      //エラーが発生した時の処理
+      let msg = SwiftData.errorMessageForCode(err);
+      print(msg)
+      return false;
+    } else {
+      return true;
+    }
+  }
+  
+  func Add_Zange(zange_id : Int, zange : String) ->Bool{
+    // 入力された懺悔を記録するメソッド
+    // sqlを準備
+    let sql = "INSERT INTO zange_mst (zange_id, zange) VALUES (?, ?)";
+    
+    // ?に入る変数をバインドして実行
+    if let err = SwiftData.executeChange( sql, withArgs: [zange_id, zange]) {
+      //エラーが発生した時の処理
+      let msg = SwiftData.errorMessageForCode(err);
+      print(msg)
+      return false;
+    } else {
+      return true;
+    }
+  }
+  
+  func Get_PicPath(gazou_id : Int) -> String{
+    // 画像IDに対して、ファイル名を返す
+    let (resultSet, err) = SD.executeQuery("SELECT url FROM gazou_mst where gazou_id = ?", withArgs: [gazou_id])
+    if err != nil {
+      //there was an error during the query, handle it here
+      return ""
+    } else {
+      for row in resultSet {
+        if let name = row["url"]?.asString() {
+          return name
+        }
+      }
+    }
+    return ""
+  }
+  
+  func Collected_Gazou(gazou_id : Int) ->Bool{
+    // 画像IDに対して、収集済みフラグを立てる
+    // sqlを準備
+    let sql = "UPDATE gazou_mst SET collect = 1 WHERE gazou_id = ? ";
+    
+    // ?に入る変数をバインドして実行
+    if let err = SwiftData.executeChange( sql, withArgs: [gazou_id]) {
+      //エラーが発生した時の処理
+      let msg = SwiftData.errorMessageForCode(err);
+      print(msg)
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
--- Zange/ZangeController.swift
+++ Zange/ZangeController.swift
@@ -9,70 +9,67 @@
 import UIKit
 
 class ZangeController: UIViewController, UITextFieldDelegate {
-    // 懺悔を入力するビュー
+  // 懺悔を入力するビュー
+  
+  // テキストフィールドの宣言
+  private var myTextField: UITextField!
+  // ボタンの宣言
+  private var myButton: UIButton!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = UIColor.blackColor()
     
-    // テキストフィールドの宣言
-    private var myTextField: UITextField!
-    // ボタンの宣言
-    private var myButton: UIButton!
+    // 「懺悔を入力」を表示するラベル
+    let myNormalLabel: UILabel = UILabel()
+    myNormalLabel.font = UIFont.systemFontOfSize(CGFloat(20))
+    myNormalLabel.text = "懺悔を入力"
+    myNormalLabel.frame = CGRect(x: 25, y: 10, width: 200, height: 150)
+    myNormalLabel.textColor = UIColor.whiteColor()
+    self.view.addSubview(myNormalLabel)
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-
-        // 「懺悔を入力」を表示するラベル
-        let myNormalLabel: UILabel = UILabel()
-        myNormalLabel.font = UIFont.systemFontOfSize(CGFloat(20))
-        myNormalLabel.text = "懺悔を入力"
-        myNormalLabel.frame = CGRect(x: 25, y: 10, width: 200, height: 150)
-        myNormalLabel.textColor = UIColor.whiteColor()
-        self.view.addSubview(myNormalLabel)
-        
-        // 懺悔を入力するテキストフィールドを宣言
-        myTextField = UITextField(frame: CGRectMake(50, 100, 250, 100))
-        myTextField.borderStyle = UITextBorderStyle.RoundedRect  // 枠線を表示
-        myTextField.delegate = self                              // デリゲートを追加
-        self.view.addSubview(myTextField)                        // ビュー画面
-        
-        // Buttonを生成する.
-        myButton = UIButton()
-        myButton.frame = CGRectMake(0,0,200,40)
-        myButton.backgroundColor = UIColor.redColor()
-        myButton.layer.masksToBounds = true
-        myButton.layer.cornerRadius = 20.0
-        myButton.layer.position = CGPoint(x: self.view.frame.width/2, y:self.view.bounds.height-50)
-        myButton.tag = 1         // タグを設定する.
-        // タイトルを設定する(通常時).
-        myButton.setTitle("懺悔ボタン", forState: UIControlState.Normal)
-        myButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-        // タイトルを設定する(ボタンがハイライトされた時).
-        myButton.setTitle("懺悔中", forState: UIControlState.Highlighted)
-        myButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Highlighted)
-        // イベントを追加する.
-        myButton.addTarget(self, action: "onClickMyButton:", forControlEvents: .TouchUpInside)
-        // ボタンをViewに追加する.
-        self.view.addSubview(myButton)
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    // 懺悔を入力するテキストフィールドを宣言
+    myTextField = UITextField(frame: CGRectMake(50, 100, 250, 100))
+    myTextField.borderStyle = UITextBorderStyle.RoundedRect  // 枠線を表示
+    myTextField.delegate = self                              // デリゲートを追加
+    self.view.addSubview(myTextField)                        // ビュー画面
     
-    internal func onClickMyButton(sender: UIButton){
-        /* ボタン押下時の処理 */
-        // 遷移するViewを定義する.
-        let myThirdViewController: UIViewController = NagusameViewController()
-        
-        // コンソールにテキストフィールドの入力値を表示
-        println("\(myTextField.text)")
-        
-        // ビューを遷移
-        self.navigationController?.pushViewController(myThirdViewController, animated: true)
-        
-    }
-
+    // Buttonを生成する.
+    myButton = UIButton()
+    myButton.frame = CGRectMake(0,0,200,40)
+    myButton.backgroundColor = UIColor.redColor()
+    myButton.layer.masksToBounds = true
+    myButton.layer.cornerRadius = 20.0
+    myButton.layer.position = CGPoint(x: self.view.frame.width/2, y:self.view.bounds.height-50)
+    myButton.tag = 1         // タグを設定する.
+    // タイトルを設定する(通常時).
+    myButton.setTitle("懺悔ボタン", forState: UIControlState.Normal)
+    myButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+    // タイトルを設定する(ボタンがハイライトされた時).
+    myButton.setTitle("懺悔中", forState: UIControlState.Highlighted)
+    myButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Highlighted)
+    // イベントを追加する.
+    myButton.addTarget(self, action: "onClickMyButton:", forControlEvents: .TouchUpInside)
+    // ボタンをViewに追加する.
+    self.view.addSubview(myButton)
     
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  internal func onClickMyButton(sender: UIButton){
+    /* ボタン押下時の処理 */
+    // 遷移するViewを定義する.
+    let myThirdViewController: UIViewController = NagusameViewController()
+    
+    // コンソールにテキストフィールドの入力値を表示
+    println("\(myTextField.text)")
+    
+    // ビューを遷移
+    self.navigationController?.pushViewController(myThirdViewController, animated: true)
+  }
 }
-
